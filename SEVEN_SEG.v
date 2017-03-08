cace1b4d33e6fddad0c13bbd2a3cab8e6d3371d5
RickWei <wlh19960621@vip.qq.com>
2017-03-08 16:01:11 +0800

    Update SEVEN_SEG.v

--- SEVEN_SEG.v
+++ SEVEN_SEG.v
@@ -37,7 +37,13 @@ module SEVEN_SEG(
                 4'd7: SEG = 8'b00011011; 
                 4'd8: SEG = 8'b00000001;  
                 4'd9: SEG = 8'b00001001;  
-                default: SEG = 8'b11111111;  
+                4'd10: SEG = 8'b00010001;
+                4'd11: SEG = 8'b11000001;
+                4'd12: SEG = 8'b01100011;
+                4'd13: SEG = 8'b10000101;
+                4'd14: SEG = 8'b01100001;
+                4'd15: SEG = 8'b01110001;
+                default: SEG = 8'b11111111;
             endcase    
         end  
 endmodule
